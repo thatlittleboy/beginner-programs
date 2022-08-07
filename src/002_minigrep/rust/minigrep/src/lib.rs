4be95dc2d7f27afdfc87184b502c87f91944369f
Jeremy Goh <jeremy_gsk@outlook.com>
2022-08-07 15:47:47 +0800

    refactor(002): use iterators for minigrep
    Chapter 13 of the rust book

--- src/002_minigrep/rust/minigrep/src/lib.rs
+++ src/002_minigrep/rust/minigrep/src/lib.rs
@@ -11,17 +11,17 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Self, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn new(mut args: impl Iterator<Item = String>) -> Result<Self, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        // NOTE: `clone()` is a cop-out method of dealing with ownership problems.
-        // We needed `clone()` because we have a slice with String elements `args`,
-        // but the `new` function doesn't own `args`. To return ownership of a Config
-        // instance and the Config instance can own its values, clone was used.
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
@@ -50,28 +50,20 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results: Vec<&str> = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
+    // Note how `query` is captured by the closure from the environment.
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results: Vec<&str> = Vec::new();
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
